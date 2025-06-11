8feb222c0f145529f1e50f32f68fd05177ddd93d
SantiMicu9 <santiago.micucci@eest3necochea.edu.ar>
2025-06-12 01:12:47 +0200

    Update index.php
    Actualización del index.php agregandole las imagenes y vinculandolo al style.css

--- index.php
+++ index.php
@@ -3,10 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
     <a href="./php/login.php">Login</a>
     <a href="./php/register.php">Registrarses</a>
+    <header>
+        <div class="logo_principal">
+            <img src="./img/logo.png" alt="Logo">
+        </div>
+        <div class="iconos">
+            <div class="alojamientos">
+                <p>Alojamientos</p>
+                <img src="./img/cama.svg" alt="Alojamientos">
+            </div>
+            <div class="vuelos">
+                <p>Vuelos</p>
+                <img src="./img/avion.svg" alt="Vuelos">
+            </div>
+            <div class="autos">
+                <p>Autos</p>
+                <img src="./img/auto.svg" alt="Autos">
+            </div>
+        </div>
+        <div class="registrarse">
+            <img src="./img/usuario.svg" alt="Registrarse">
+            <a href="./php/register.php">Registrarse</a>
+        </div>
+    </header>
+
+    <div class="container">
+
+    </div>
+
+    <footer>
+        <div class="logo_footer">
+            <img src="./img/logo.png" alt="Logo">
+        </div>
+        <div class="atencion_al_cliente">
+            <p>Atención al cliente</p>
+            <p>+54 9 2262 34-9131</p>
+        </div>
+    </footer>
 </body>
 </html>
