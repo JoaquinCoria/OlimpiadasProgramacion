8003940a97a549b5b46cb4cd645aaebc35784e7d
JoaquinCoria <joaquin.i.coria@gmail.com>
2025-06-14 18:20:40 -0300

    Actualización de funciones en carrito
    Se traen cambios de otras branch a Carrito

--- php/carrito.php
+++ php/carrito.php
@@ -10,9 +10,18 @@ if (!isset($_SESSION['nombreUsuario'])) {
 // Obtener el ID del usuario
 
 // Obtener los productos del carrito del usuario
-$resultadoProductos = mysqli_query($conexion, "SELECT fkIdProducto FROM carrito WHERE fkIdUsuario = '".$_SESSION['idUsuario']."';");
 
+$resultadoProductos =  mysqli_query($conexion, "SELECT fkIdProducto FROM carrito WHERE fkIdUsuario = '".$_SESSION['idUsuario']."';");
+$row = mysqli_fetch_array($resultadoProductos, MYSQLI_ASSOC);
+var_dump($resultadoProductos);
+foreach($resultadoProductos as $i=>$idProducto)
+{
 
+    $datosProductos[$i] = mysqli_fetch_assoc(mysqli_query($conexion, "SELECT * FROM producto WHERE idProducto = ".$idProducto.";"));
+}
+var_dump($datosProductos);
+
+//query = "SELECT * FROM producto WHERE idProducto = "
 
 //Eliminar productos
 
