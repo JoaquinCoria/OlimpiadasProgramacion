0fbaec19980400e1f5273581bac8b9cf3be51131
JoaquinCoria <joaquin.i.coria@gmail.com>
2025-06-13 23:11:19 -0300

    Se agrega agregarProductoCarrito.php y errores de login.php
    Se agrega agregarProductoCarrito.php y su funcionalidad terminada, también se corrigieron errores después de hacer merge de cambios de la branch index a carrito

--- php/agregarProductoCarrito.php
+++ php/agregarProductoCarrito.php
@@ -1,15 +1,10 @@
 <?php
     include_once('conexion.php');
     session_start();
-    print_r($_POST['producto']);
-    echo "<br>";
-    print_r(sizeof($_POST['producto']));
-     echo "<br>";
+
     foreach($_POST['producto'] as $idProducto){
         $query = "INSERT INTO carrito(fkIdUsuario, fkIdProducto, precioUnidad) VALUE (".$_SESSION['idUsuario'].", ".$idProducto.", (SELECT precio FROM producto WHERE idProducto = ".$idProducto."));";	
         $resultado = mysqli_query($conexion, $query);
     }
-//     $query = "INSERT INTO carrito(fkIdUsuario, fkIdProducto, precioTotal, precioUnidad) VALUE (".$_SESSION['idUsuario'].", 	
-// "
-//     $_POST['']
+    echo "<script>alert('Productos agregados correctamente a carrito'); window.location = '../index.php'</script>";
 ?>
