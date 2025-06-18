72ace10710e1ffe608c3b83514bb4587c41eda0f
CiroBlanco <santino3191difi@gmail.com>
2025-06-17 21:22:31 -0300

    Style de datosPedido.php

--- php/datosPedido.php
+++ php/datosPedido.php
@@ -12,7 +12,10 @@
     // Guarda momentaneamente el valor de $_POST['producto'] en $_SESSION
     if(isset($_POST['producto']))
     {
+<<<<<<< Updated upstream
         // Elimina el $_SESSION['idProducto'] si había un idProducto antes 
+=======
+>>>>>>> Stashed changes
         unset($_SESSION['idProducto']);
         $_SESSION['idProducto'] = $_POST['producto'];
     }
@@ -46,7 +49,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/datosPedido.css">
+    <link rel="stylesheet" href="../style/datosPedido.css?3">
     <title>Datos del pedido</title>
 </head>
 <body>
@@ -105,9 +108,13 @@
                 }
             }
             ?>
-            <button type="submit" class="btnPedir" name="pedir" value="pedir">
-                Agregar al carrito
-            </button>
+            <div class="btnPedir">
+                <a href="../index.php">Volver</a>
+                <button type="submit"  name="pedir" value="pedir">
+                    Agregar al carrito
+                </button>
+                
+            </div>
         </form>   
     </div>
 </body>
