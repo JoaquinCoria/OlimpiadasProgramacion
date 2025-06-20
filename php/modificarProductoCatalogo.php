9b8d324d34cb8a9edb9bdf58fc036d36d6df1fed
JoaquinCoria <joaquin.i.coria@gmail.com>
2025-06-19 21:10:59 -0300

    Modificar producto
    Se agrega la nueva opción para admin, al tocar un producto en el index permite modificar y eliminar productos

--- php/modificarProductoCatalogo.php
+++ php/modificarProductoCatalogo.php
@@ -1,4 +1,10 @@
 <?php
+    session_start();
+    include_once('conexion.php');
+    if($_SESSION['admin'] == FALSE || !isset($_POST['producto'])){
+        //header('location:../index.php');
+    }
+    $informacionProducto = mysqli_query($conexion, "SELECT * FROM producto WHERE idProducto = ".$_POST['producto'].";")->fetch_assoc();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,6 +14,19 @@
     <title>Producto vendedor</title>
 </head>
 <body>
-    asdadopasdko
+    <div class="container">
+        <?php echo '
+        <div class="tarjetaProducto">
+            <h1>'.$informacionProducto["nombre"].'</h1>
+            <p>'.$informacionProducto["descripcion"].'</p>
+            ';
+            echo '<p>Precio: $'.$informacionProducto['precio'].'</p>
+        </div>
+        ';?>
+        <form action="modificarProductoCatalogo.php" method="post">
+            <input type="submit" name="btnVendedor" value="Modificar">
+            <input type="submit" name="btnVendedor" value="Eliminar">
+        </form>
+    </div>
 </body>
 </html>
