8e9d7f08c4c24e1f4f9a2a684b3072b39d2b417d
SantiMicu9 <santiago.micucci@eest3necochea.edu.ar>
2025-06-21 01:08:38 -0300

    Estilos productoAdmin y agregarProductoCatalogo
    CSS para productoAdmin.php y agregarProductoCatalogo.php

--- php/agregarProductoCatalogo.php
+++ php/agregarProductoCatalogo.php
@@ -37,6 +37,7 @@ if (isset($_POST['agregar'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/agregarProductoCatalogo.css">
     <title>Agregar producto</title>
 </head>
 <body>
